fb1e45dc1c6bc89231f1d0246e2bdaa6c3d83e5a
Millorco <camillo.fagnano@gmail.com>
2025-02-16 14:41:37 +0100

    Update up_server.js

--- htdocs/luci-static/resources/view/webcam/up_server.js
+++ htdocs/luci-static/resources/view/webcam/up_server.js
@@ -1,1 +1,30 @@
+'use strict';
+'require view';
+'require form';
 
+return view.extend({
+	render: function() {
+		let m, s, o;
+		m = new form.Map('webcam', _(''),
+			_('Example Form Configuration.'));
+
+		s = m.section(form.TypedSection, 'server', _('Upload Server'));
+		s.anonymous = true;	
+
+		s.option(form.Value, 'upload_server', _('Server'),
+			_('Input for the first option'));
+								
+
+		s.option(form.Value, 'upload_directory', _('Directory'),
+			_('Input for the first option'));
+
+		s.option(form.Value, 'upload_username', _('Username'),
+			_('Input for the first option'));
+
+		o = s.option(form.Value, 'upload_password', _('Password'),
+			_('Input for a password (storage on disk is not encrypted)'));
+		o.password = true;
+
+		return m.render();
+	},
+});
