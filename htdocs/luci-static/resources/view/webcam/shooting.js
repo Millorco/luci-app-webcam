2931cba5e78d12467ff663f5e5cef7f2922ad4d5
Millorco <camillo.fagnano@gmail.com>
2025-02-16 18:01:20 +0100

    Update shooting.js

--- htdocs/luci-static/resources/view/webcam/shooting.js
+++ htdocs/luci-static/resources/view/webcam/shooting.js
@@ -5,10 +5,9 @@
 return view.extend({
 	render: function() {
 		let m, s, o;
-		m = new form.Map('webcam', _(''),
-			_('Example Form Configuration.'));
+		m = new form.Map('webcam', _(''));
 
-		s = m.section(form.TypedSection, 'shooting', _('Generl shooting'));
+		s = m.section(form.TypedSection, 'shooting', _('Common Shooting Settings'));
 		s.anonymous = true;
 		
 		o = s.option(form.ListValue, 'imageformat', _('Image Format'),
