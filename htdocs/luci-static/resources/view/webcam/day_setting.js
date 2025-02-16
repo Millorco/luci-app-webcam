6ef12523b95a2375d91a1ed575dccbb6849bc854
Millorco <camillo.fagnano@gmail.com>
2025-02-16 13:21:05 +0100

    Update day_setting.js

--- htdocs/luci-static/resources/view/webcam/day_setting.js
+++ htdocs/luci-static/resources/view/webcam/day_setting.js
@@ -1,1 +1,29 @@
+'use strict';
+'require form';
+'require fs';
+'require uci';
+'require ui';
+'require view';
+
+return view.extend({
+	render: function() {
+		let m, s, o;
+		m = new form.Map('day_setting', _(''),
+			_('Example Form Configuration.'));
+
+		s.option(form.Value, 'latitude', _('Latitude'),
+			_('Input for the first option'));
+
+		s.option(form.Value, 'longitude', _('Longitude'),
+			_('Input for the first option'));
+			
+		o = s.option(form.Flag, 'faraday', _('Use Faraday'),
+			_('A boolean option'));
+		o.default = '0';
+		o.rmempty = false;
+
+		return m.render();
+	},
+});
+
 
