99e2c945316aff2f05cd19ceed1f833f8c178100
Millorco <camillo.fagnano@gmail.com>
2025-06-29 15:32:09 +0200

    Update setting.js

--- htdocs/luci-static/resources/view/webcam/setting.js
+++ htdocs/luci-static/resources/view/webcam/setting.js
@@ -30,6 +30,42 @@ return view.extend({
 		o = s.option(form.Flag, 'heating', _('Have you installed a heater with a fan?'));
 		o.default = o.enabled;
 		o.rmempty = false;
+		
+		
+		
+		s = m.section(form.TypedSection, 'shooting', _('Common Shooting Settings'));
+		s.anonymous = true;
+		
+		o = s.option(form.ListValue, 'imageformat', _('Image Format'),
+			_('A select option'));
+		o.placeholder = 'placeholder';
+		o.value('0', 'Large Fine JPEG');
+		o.value('1', 'Large Normal JPEG');
+		o.value('2', 'Medium Fine JPEG');
+		o.value('3', 'Medium Normal JPEG');
+		o.value('4', 'Small Fine JPEG');
+		o.value('5', 'Small Normal JPEG');
+		o.value('6', 'Smaller JPEG');
+		o.value('7', 'Tiny JPEG');
+		o.value('8', 'RAW + Large Fine JPEG');
+		o.value('9', 'RAW');
+		o.rmempty = false;
+		o.editable = true;
+		
+		o = s.option(form.ListValue, 'whitebalance', _('Whitebalance'),
+			_('A select option'));
+		o.placeholder = 'placeholder';
+		o.value('0', 'Auto');
+		o.value('1', 'AWB White');
+		o.value('2', 'Daylight');
+		o.value('3', 'Shadow');
+		o.value('4', 'Cloudy');
+		o.value('5', 'Tungsten');
+		o.value('6', 'Fluorescent');
+		o.value('7', 'Flash');
+		o.value('8', 'Manual');
+		o.rmempty = false;
+		o.editable = true;
 
 		return m.render();
 	},
