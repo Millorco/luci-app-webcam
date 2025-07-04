c08b141cf43d2137d68d6d2a38209e0131df6b97
Millorco <camillo.fagnano@gmail.com>
2025-07-04 08:43:39 +0200

    Update webcam.ino

--- webcam.ino
+++ webcam.ino
@@ -1,7 +1,7 @@
 #include <Wire.h>
 #include <SoftwareSerial.h>
 
-SoftwareSerial PCSerial(6, 7); // RX, TX
+SoftwareSerial WebcamSerial(6, 7); // RX, TX
 
 const unsigned long HEARTBEAT_TIMEOUT = 600000; // 10 minuti in millisecondi
 unsigned long lastHeartbeatTime = 0;
@@ -10,31 +10,28 @@ String inputString = "";
 void setup() {
 	Wire.begin();
 	Serial.begin(9600);
-	PCSerial.begin(9600);
+	WebcamSerial.begin(9600);
 	lastHeartbeatTime = millis(); // Inizializza il timer all'avvio
 	pinMode(2, OUTPUT); // sets pin 2 as output for Camera Power
 	pinMode(3, OUTPUT); // sets pin 3 as output for Heating
 	pinMode(4, OUTPUT); // sets pin 4 as output for PC
 	pinMode(5, OUTPUT); // sets pin 5 as output for Fun
-	pinMode(10, OUTPUT); // sets pin 10 as output TEST
-	pinMode(13, OUTPUT); // sets pin 13 as output TEST
 	
 	delay(1000); // let serial console settle
-
 }
 
 
 void loop() {
 	
 	// Controllo del timeout dell'heartbeat
-	if (millis() - lastHeartbeatTime > HEARTBEAT_TIMEOUT) {
 	// Se non riceve un segnale di Heartbeat da 10 minuti resettta in PC
+	if (millis() - lastHeartbeatTime > HEARTBEAT_TIMEOUT) {
 		resetFunc(); //funzione di reset del PC
 	}	
 	
 	// Read and execute commands from serial port
-	if (PCSerial.available()) {  // check for incoming serial data
-		String command = PCSerial.readString();  // read command from serial port
+	if (PCSerial.available()) {
+		String command = PCSerial.readString();
 	
 	if (command == "b") { // Heartbeat signal
 			lastHeartbeatTime = millis();
@@ -54,18 +51,8 @@ void loop() {
 			digitalWrite(5, HIGH);
 	} else if (command == "f") {  // turn off Fun
 			digitalWrite(5, LOW);
-	} else if (command == "X") {  // turn on TEST
-			digitalWrite(10, HIGH);
-	} else if (command == "x") {  // turn off TEST
-			digitalWrite(10, LOW);
-	} else if (command == "T") {  // turn on LED
-			digitalWrite(13, HIGH);
-	} else if (command == "t") {  // turn off LED
-			digitalWrite(13, LOW);
-	} else if (command == "m") {  // turn off LED
-			PCSerial.print("Lettera M");
-	} else if (command == "n") {  // turn off LED
-			Serial.print("Lettera N");
+	}  else if (command == "n") {  // Test Software Serial
+			WebcamSerial.print("Software Serial OK");
 	} 
 }
 }
