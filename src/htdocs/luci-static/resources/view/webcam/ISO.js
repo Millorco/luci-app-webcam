440e2316c6db4a7f20fe8e8860951daa6a925749
Millorco <camillo.fagnano@gmail.com>
2025-07-06 13:51:15 +0200

    Update ISO.js

--- src/htdocs/luci-static/resources/view/webcam/ISO.js
+++ src/htdocs/luci-static/resources/view/webcam/ISO.js
@@ -3,32 +3,24 @@
 'require fs';
 'require ui';
 
-var isReadonlyView = !L.hasViewPermission() || null;
-
 return view.extend({
 	load: function() {
 		return L.resolveDefault(fs.read('/etc/crontabs/root'), '');
 	},
 
-	handleSave: function(ev) {
-		var value = (document.querySelector('textarea').value || '').trim().replace(/\r\n/g, '\n') + '\n';
-
-		return fs.write('/etc/crontabs/root', value).then(function(rc) {
-			document.querySelector('textarea').value = value;
-			ui.addNotification(null, E('p', _('Contents have been saved.')), 'info');
-
-			return fs.exec('/etc/init.d/cron', [ 'reload' ]);
-		}).catch(function(e) {
-			ui.addNotification(null, E('p', _('Unable to save contents: %s').format(e.message)));
-		});
-	},
-
 	render: function(crontab) {
 		return E([
-			E('p', {}, E('textarea', { 'style': 'width:100%', 'rows': 5, 'disabled': isReadonlyView }, [ crontab != null ? crontab : '' ]))
+			E('h3', {}, _('Crontab Contents (Read-Only)')),
+			E('p', {}, E('textarea', { 
+				'style': 'width:100%', 
+				'rows': 10, 
+				'readonly': true,
+				'disabled': true
+			}, [ crontab != null ? crontab : _('No crontab entries found') ]))
 		]);
 	},
 
+	handleSave: null,
 	handleSaveApply: null,
 	handleReset: null
 });
